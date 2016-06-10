e74562edcf60239cb400974a98324191053a300f
Adrien Cassagne <adrien.cassagne@inria.fr>
2016-06-10 11:24:06 +0200

    Add a new example.

--- tests/operator.cpp
+++ tests/operator.cpp
@@ -9,7 +9,7 @@ int main(int argc, char** argv)
 	std::random_device rd;
 	std::mt19937 g(rd());
 
-	using type = short;
+	using type = float;
 
 	type t_1[mipp::nElReg<type>()];
 	for (auto i = 0; i < mipp::nElReg<type>(); i++) t_1[i] = i+1;
@@ -96,6 +96,12 @@ int main(int argc, char** argv)
 
 		out = in_1 / in_2;
 		std::cout << "Output vectors (in_1 / in_2): " << std::endl;
+		std::cout << "out  = " << out << std::endl;
+		std::cout << std::endl;
+
+		auto two = mipp::Reg<type>(2);
+		out = in_1 + in_2 * two;
+		std::cout << "Output vectors (in_1 + in_2 * two): " << std::endl;
 		std::cout << "out  = " << out << std::endl;
 		std::cout << std::endl;
 	}
