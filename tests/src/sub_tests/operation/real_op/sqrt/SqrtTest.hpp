515e0f493217c3cb227cc4081620d17bd7b847d3
Adrien Cassagne <adrien.cassagne@inria.fr>
2017-07-04 09:17:15 +0200

    Add tests for sqrt and rsqrt instr.

---
+++ tests/src/sub_tests/operation/real_op/sqrt/SqrtTest.hpp
@@ -0,0 +1,65 @@
+#ifndef SQRTTEST_HPP_
+#define SQRTTEST_HPP_
+
+#include <cppunit/extensions/HelperMacros.h>
+
+class SqrtTest : public CppUnit::TestFixture
+{
+	CPPUNIT_TEST_SUITE(SqrtTest);
+
+	CPPUNIT_TEST(test_reg_sqrt_float);
+	CPPUNIT_TEST(test_reg_sqrt_double);
+
+	CPPUNIT_TEST(test_Reg_sqrt_float);
+	CPPUNIT_TEST(test_Reg_sqrt_double);
+
+	CPPUNIT_TEST(test_reg_maskz_sqrt_float);
+	CPPUNIT_TEST(test_reg_maskz_sqrt_double);
+
+	CPPUNIT_TEST(test_Reg_maskz_sqrt_float);
+	CPPUNIT_TEST(test_Reg_maskz_sqrt_double);
+
+	CPPUNIT_TEST(test_reg_mask_sqrt_float);
+	CPPUNIT_TEST(test_reg_mask_sqrt_double);
+
+	CPPUNIT_TEST(test_Reg_mask_sqrt_float);
+	CPPUNIT_TEST(test_Reg_mask_sqrt_double);
+
+	CPPUNIT_TEST_SUITE_END();
+
+public:
+	void setUp();
+	void tearDown();
+
+	template <typename T>
+	void test_reg_sqrt();
+	void test_reg_sqrt_float();
+	void test_reg_sqrt_double();
+
+	template <typename T>
+	void test_Reg_sqrt();
+	void test_Reg_sqrt_float();
+	void test_Reg_sqrt_double();
+
+	template <typename T>
+	void test_reg_maskz_sqrt();
+	void test_reg_maskz_sqrt_float();
+	void test_reg_maskz_sqrt_double();
+
+	template <typename T>
+	void test_Reg_maskz_sqrt();
+	void test_Reg_maskz_sqrt_float();
+	void test_Reg_maskz_sqrt_double();
+
+	template <typename T>
+	void test_reg_mask_sqrt();
+	void test_reg_mask_sqrt_float();
+	void test_reg_mask_sqrt_double();
+
+	template <typename T>
+	void test_Reg_mask_sqrt();
+	void test_Reg_mask_sqrt_float();
+	void test_Reg_mask_sqrt_double();
+};
+
+#endif // SQRTTEST_HPP_
