f11d23777c60a033e7636517e2a5248a54afc978
Adrien Cassagne <adrien.cassagne@inria.fr>
2017-12-14 14:54:04 +0100

    Add testz missing tests.

--- tests/src/sub_tests/operation/reduction/testz/TestzTest.cpp
+++ tests/src/sub_tests/operation/reduction/testz/TestzTest.cpp
@@ -216,3 +216,178 @@ void TestzTest::test_Msk_testz_int8 (){ test_Msk_testz_int <int8_t >(); }
 void TestzTest::test_Msk_testz_int16(){ test_Msk_testz_int <int16_t>(); }
 void TestzTest::test_Msk_testz_int32(){ test_Msk_testz_int <int32_t>(); }
 void TestzTest::test_Msk_testz_int64(){ test_Msk_testz_int <int64_t>(); }
+
+template <typename T>
+void TestzTest::test_reg_testz1_int()
+{
+	try
+	{
+		constexpr int N = mipp::N<T>();
+		T inputs1[N];
+		std::mt19937 g;
+		std::uniform_int_distribution<T> dis((T)std::numeric_limits<T>::min(), (T)std::numeric_limits<T>::max());
+
+		for (auto j = 0; j < 100; j++)
+		{
+			for (auto i = 0; i < N; i++)
+			{
+				switch (j%2)
+				{
+					case 0:  inputs1[i] = dis(g); break;
+					case 1:  inputs1[i] = 0;      break;
+					default: inputs1[i] = 0;      break;
+				}
+			}
+
+			mipp::reg r1 = mipp::load<T>(inputs1);
+			auto val = mipp::testz<T>(r1);
+
+			auto res = inputs1[0];
+			for (auto i = 1; i < N; i++)
+				res |= inputs1[i];
+
+			CPPUNIT_ASSERT_EQUAL(res == 0 ? 1 : 0, val);
+		}
+	}
+	catch(std::exception &e)
+	{
+		CPPUNIT_FAIL(e.what());
+	}
+}
+
+void TestzTest::test_reg_testz1_int8 (){ test_reg_testz1_int <int8_t >(); }
+void TestzTest::test_reg_testz1_int16(){ test_reg_testz1_int <int16_t>(); }
+void TestzTest::test_reg_testz1_int32(){ test_reg_testz1_int <int32_t>(); }
+void TestzTest::test_reg_testz1_int64(){ test_reg_testz1_int <int64_t>(); }
+
+template <typename T>
+void TestzTest::test_Reg_testz1_int()
+{
+	try
+	{
+		constexpr int N = mipp::N<T>();
+		T inputs1[N];
+		std::mt19937 g;
+		std::uniform_int_distribution<T> dis((T)std::numeric_limits<T>::min(), (T)std::numeric_limits<T>::max());
+
+		for (auto j = 0; j < 100; j++)
+		{
+			for (auto i = 0; i < N; i++)
+			{
+				switch (j%2)
+				{
+					case 0:  inputs1[i] = dis(g); break;
+					case 1:  inputs1[i] = 0;      break;
+					default: inputs1[i] = 0;      break;
+				}
+			}
+
+			mipp::Reg<T> r1 = inputs1;
+			auto val = mipp::testz(r1);
+
+			auto res = inputs1[0];
+			for (auto i = 1; i < N; i++)
+				res |= inputs1[i];
+
+			CPPUNIT_ASSERT_EQUAL(res == 0 ? 1 : 0, val);
+		}
+	}
+	catch(std::exception &e)
+	{
+		CPPUNIT_FAIL(e.what());
+	}
+}
+
+void TestzTest::test_Reg_testz1_int8 (){ test_Reg_testz1_int <int8_t >(); }
+void TestzTest::test_Reg_testz1_int16(){ test_Reg_testz1_int <int16_t>(); }
+void TestzTest::test_Reg_testz1_int32(){ test_Reg_testz1_int <int32_t>(); }
+void TestzTest::test_Reg_testz1_int64(){ test_Reg_testz1_int <int64_t>(); }
+
+template <typename T>
+void TestzTest::test_msk_testz1_int()
+{
+	try
+	{
+		constexpr int N = mipp::N<T>();
+		bool inputs1[N];
+		std::mt19937 g;
+		std::uniform_int_distribution<T> dis(0, 1);
+
+		for (auto j = 0; j < 100; j++)
+		{
+			for (auto i = 0; i < N; i++)
+			{
+				switch (j%3)
+				{
+					case 0:  inputs1[i] = dis(g) ? true : false; break;
+					case 1:  inputs1[i] = true;                  break;
+					case 2:  inputs1[i] = false;                 break;
+					default: inputs1[i] = false;                 break;
+				}
+			}
+
+			mipp::msk r1 = mipp::set<N>(inputs1);
+			auto val = mipp::testz<N>(r1);
+
+			auto res = inputs1[0];
+			for (auto i = 1; i < N; i++)
+				res |= inputs1[i];
+
+			CPPUNIT_ASSERT_EQUAL(res == 0 ? 1 : 0, val);
+		}
+	}
+	catch(std::exception &e)
+	{
+		CPPUNIT_FAIL(e.what());
+	}
+}
+
+void TestzTest::test_msk_testz1_int8 (){ test_msk_testz1_int <int8_t >(); }
+void TestzTest::test_msk_testz1_int16(){ test_msk_testz1_int <int16_t>(); }
+void TestzTest::test_msk_testz1_int32(){ test_msk_testz1_int <int32_t>(); }
+void TestzTest::test_msk_testz1_int64(){ test_msk_testz1_int <int64_t>(); }
+
+template <typename T>
+void TestzTest::test_Msk_testz1_int()
+{
+	try
+	{
+		constexpr int N = mipp::N<T>();
+		bool inputs1[N];
+		std::mt19937 g;
+		std::uniform_int_distribution<T> dis(0, 1);
+
+		for (auto j = 0; j < 100; j++)
+		{
+			for (auto i = 0; i < N; i++)
+			{
+				switch (j%3)
+				{
+					case 0:  inputs1[i] = dis(g) ? true : false; break;
+					case 1:  inputs1[i] = true;                  break;
+					case 2:  inputs1[i] = false;                 break;
+					default: inputs1[i] = false;                 break;
+				}
+			}
+
+			mipp::Msk<N> r1 = inputs1;
+			auto val = mipp::testz(r1);
+
+			auto res = inputs1[0];
+			for (auto i = 1; i < N; i++)
+				res |= inputs1[i];
+
+			CPPUNIT_ASSERT_EQUAL(res == 0 ? 1 : 0, val);
+		}
+	}
+	catch(std::exception &e)
+	{
+		CPPUNIT_FAIL(e.what());
+	}
+}
+
+void TestzTest::test_Msk_testz1_int8 (){ test_Msk_testz1_int <int8_t >(); }
+void TestzTest::test_Msk_testz1_int16(){ test_Msk_testz1_int <int16_t>(); }
+void TestzTest::test_Msk_testz1_int32(){ test_Msk_testz1_int <int32_t>(); }
+void TestzTest::test_Msk_testz1_int64(){ test_Msk_testz1_int <int64_t>(); }
+
