cad9008addd95e663902066d5678a1054ca90493
Adrien Cassagne <adrien.cassagne@inria.fr>
2018-06-01 15:46:41 +0200

    Fix MSVC cmd lines.

--- ci/build-windows-x86-msvc.bat
+++ ci/build-windows-x86-msvc.bat
@@ -10,13 +10,13 @@ cd tests
 
 rem set build_root=build_windows_x86_msvc
 call :compile "build_windows_x86_msvc_nointr"   "-DMIPP_NO_INTRINSICS"
-call :compile "build_windows_x86_msvc_sse2"     "/arch:SSE2"
-call :compile "build_windows_x86_msvc_sse3"     "/arch:SSE3"
-call :compile "build_windows_x86_msvc_ssse3"    "/arch:SSSE3"
-call :compile "build_windows_x86_msvc_sse4_1"   "/arch:SSE4.1"
-call :compile "build_windows_x86_msvc_sse4_2"   "/arch:SSE4.2"
-call :compile "build_windows_x86_msvc_avx"      "/arch:AVX"
-call :compile "build_windows_x86_msvc_avx2_fma" "/arch:AVX2"
+call :compile "build_windows_x86_msvc_sse2"     "/arch:SSE2   -D__SSE__ -D__SSE2__"
+call :compile "build_windows_x86_msvc_sse3"     "/arch:SSE3   -D__SSE__ -D__SSE2__ -D__SSE3__"
+call :compile "build_windows_x86_msvc_ssse3"    "/arch:SSSE3  -D__SSE__ -D__SSE2__ -D__SSE3__ -D__SSSE3__"
+call :compile "build_windows_x86_msvc_sse4_1"   "/arch:SSE4.1 -D__SSE__ -D__SSE2__ -D__SSE3__ -D__SSSE3__ -D__SSE4_1__"
+call :compile "build_windows_x86_msvc_sse4_2"   "/arch:SSE4.2 -D__SSE__ -D__SSE2__ -D__SSE3__ -D__SSSE3__ -D__SSE4_1__ -D__SSE4_2__"
+call :compile "build_windows_x86_msvc_avx"      "/arch:AVX    -D__SSE__ -D__SSE2__ -D__SSE3__ -D__SSSE3__ -D__SSE4_1__ -D__SSE4_2__ -D__AVX__"
+call :compile "build_windows_x86_msvc_avx2_fma" "/arch:AVX2   -D__SSE__ -D__SSE2__ -D__SSE3__ -D__SSSE3__ -D__SSE4_1__ -D__SSE4_2__ -D__AVX__ -D__AVX2__ -D__FMA__"
 rem call :compile "build_windows_x86_msvc_avx512f"  "/arch:AVX512"
 
 exit /B %ERRORLEVEL%
