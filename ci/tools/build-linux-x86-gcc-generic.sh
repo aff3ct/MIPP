124b6dffffda4ce8773ac5737d6a5d4acddcb8ba
Florian Ritterhoff <ritterho@hm.edu>
2020-01-04 17:27:38 +0100

    tests: add missing script

---
+++ ci/tools/build-linux-x86-gcc-generic.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#set -x
+
+function compile {
+
+    cd tests
+    build_root=build_linux_x86_gcc
+	build="${build_root}_$1"
+	mkdir $build
+	cd $build
+	cmake .. -G"Unix Makefiles" -DCMAKE_CXX_COMPILER=g++ -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-Wall -funroll-loops -finline-functions $2"
+	rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+	make -j $THREADS
+	rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+	cd ..
+}
