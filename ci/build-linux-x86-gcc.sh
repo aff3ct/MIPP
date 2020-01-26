8e4b5f66f1b2650885faeafcbc5c9a0e8d7ff5bf
Adrien Cassagne <adrien.cassagne@inria.fr>
2020-01-26 10:58:16 +0100

    CI: compile Linux binaries in static to avoid compatibility issues.

--- ci/build-linux-x86-gcc.sh
+++ ci/build-linux-x86-gcc.sh
@@ -5,7 +5,7 @@ function compile {
 	build=$1
 	mkdir $build
 	cd $build
-	cmake .. -G"Unix Makefiles" -DCMAKE_CXX_COMPILER=g++ -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-Wall -funroll-loops -finline-functions $2"
+	cmake .. -G"Unix Makefiles" -DCMAKE_CXX_COMPILER=g++ -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-Wall -funroll-loops -finline-functions $2" -DCMAKE_EXE_LINKER_FLAGS="-static -static-libgcc -static-libstdc++"
 	rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 	make -j $THREADS
 	rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
