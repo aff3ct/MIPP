4c7fdb66db67f111603d88ee0e13891dfd980668
Adrien Cassagne <adrien.cassagne@inria.fr>
2021-05-26 15:57:41 +0200

    CI: use `msbuild` instead of `devenv`.

--- ci/build-windows-x86-msvc.bat
+++ ci/build-windows-x86-msvc.bat
@@ -27,8 +27,8 @@ mkdir %build%
 cd %build%
 cmake .. -G"Visual Studio 15 2017 Win64" -DCMAKE_CXX_FLAGS="-D_CRT_SECURE_NO_DEPRECATE /EHsc /MP%THREADS% %params%"
 if %ERRORLEVEL% neq 0 exit %ERRORLEVEL%
-devenv /build Release MIPP_tests.sln
-rem msbuild MIPP_tests.sln /t:Build /p:Configuration=Release
+rem devenv /build Release MIPP_tests.sln
+msbuild MIPP_tests.sln /t:Build /p:Configuration=Release
 if %ERRORLEVEL% neq 0 exit %ERRORLEVEL%
 move bin\Release\* bin\
 rmdir bin\Release\
