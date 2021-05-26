5a6d9eaa30638352f76ab18c2bffe1d248221dfb
Adrien Cassagne <adrien.cassagne@inria.fr>
2021-05-26 15:51:17 +0200

    CI: remove dependency to cppcheck for the builds + fix cppcheck format.

--- ci/analysis-cppcheck.sh
+++ ci/analysis-cppcheck.sh
@@ -7,14 +7,14 @@ mkdir cppcheck
 #cppcheck --suppress=missingIncludeSystem -I./src/ --force --enable=all --std=c++11 -U_MSC_VER ./src/ 2> cppcheck_all.log
 find .\/src\/ -type f -follow -print | grep "[.]h$\|[.]hpp$\|[.]hxx$\|[.]cpp$" > src_files.txt
 cppcheck --language=c++ --suppress=missingIncludeSystem --force --enable=all --std=c++11 -U_MSC_VER --file-list=src_files.txt 2> cppcheck/cppcheck_all.log
-cat cppcheck/cppcheck_all.log | grep "(error)"          > cppcheck/cppcheck_error.log
-cat cppcheck/cppcheck_all.log | grep "(warning)"        > cppcheck/cppcheck_warning.log
-cat cppcheck/cppcheck_all.log | grep "(performance)"    > cppcheck/cppcheck_performance.log
-cat cppcheck/cppcheck_all.log | grep "(style)"          > cppcheck/cppcheck_style.log
-cat cppcheck/cppcheck_all.log | grep "(portability)"    > cppcheck/cppcheck_portability.log
-cat cppcheck/cppcheck_all.log | grep "(information)"    > cppcheck/cppcheck_information.log
-cat cppcheck/cppcheck_all.log | grep "(unusedFunction)" > cppcheck/cppcheck_unusedFunction.log
-cat cppcheck/cppcheck_all.log | grep "(missingInclude)" > cppcheck/cppcheck_missingInclude.log
+cat cppcheck/cppcheck_all.log | grep "error:"          > cppcheck/cppcheck_error.log
+cat cppcheck/cppcheck_all.log | grep "warning:"        > cppcheck/cppcheck_warning.log
+cat cppcheck/cppcheck_all.log | grep "performance:"    > cppcheck/cppcheck_performance.log
+cat cppcheck/cppcheck_all.log | grep "style:"          > cppcheck/cppcheck_style.log
+cat cppcheck/cppcheck_all.log | grep "portability:"    > cppcheck/cppcheck_portability.log
+cat cppcheck/cppcheck_all.log | grep "information:"    > cppcheck/cppcheck_information.log
+cat cppcheck/cppcheck_all.log | grep "unusedFunction:" > cppcheck/cppcheck_unusedFunction.log
+cat cppcheck/cppcheck_all.log | grep "missingInclude:" > cppcheck/cppcheck_missingInclude.log
 
 COUNT=$(wc -l < cppcheck/cppcheck_error.log )
 
