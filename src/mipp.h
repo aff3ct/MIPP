bdaa7c627486882706436b05e0f8f16e488ffcf0
Adrien Cassagne <adrien.cassagne@inria.fr>
2021-05-26 23:26:25 +0200

    Fix possible division by 0 (again).

--- src/mipp.h
+++ src/mipp.h
@@ -449,7 +449,7 @@ template <typename T>
 inline bool isAligned(const T *ptr)
 {
 #ifdef MIPP_ALIGNED_LOADS
-	return RegisterSizeBit ? (((uintptr_t)ptr) % (RegisterSizeBit / 8)) == 0 : true;
+	return RegisterSizeBit >= 8 ? (((uintptr_t)ptr) % (RegisterSizeBit / 8)) == 0 : true;
 #else
 	return true;
 #endif
