eb652b29bd7ceddb8dfd3f30176692af7b9f637b
dendanil <lamia.dendani@ifpen.fr>
2023-06-19 13:16:26 +0200

    add argument all to generate mipp avx2 and avx512

--- gen/gen_mipp_v2.py
+++ gen/gen_mipp_v2.py
@@ -1,26 +1,36 @@
 
 import sys
+import os
 
-from gen_mipp_avx import  *
-from gen_mipp_avx512 import  *
+path = os.getcwd()
 
+sys.path.insert(1,path +'/avx512_gen')
+sys.path.insert(1,path + '/avx_gen')
 
-
+from avx_gen import gen_mipp_avx
+from avx512_gen import gen_mipp_avx512
+dir = 
+/work/dendanil/MIPP/include/avx512
+print(path)
 
 for arg in sys.argv:
 	if(arg == "gen_mipp_v2.py"):
 		continue
 	if(arg == "avx2"):
-		gen_mipp_avx()
+		gen_mipp_avx.gen_mipp_avx()
 	elif (arg == "avx512"):
-		gen_mipp_avx512()
-	# ajouter un argument avx2 & avx512 & sve 
+		gen_mipp_avx512.gen_mipp_avx512()
+	elif (arg == "all"):
+		gen_mipp_avx512.gen_mipp_avx512()
+		print("//////////////////////////////////////////////////////////")
+		gen_mipp_avx.gen_mipp_avx()
 	elif (arg == "--help"):
 		print("avx: mipp generator for avx2 simd")
 		print("avx512: mipp generator for avx512 simd")
 		print("version: mipp version")
 	elif (arg == "--version"):
 		print("MIPP.V2 version 2023")
+	
 	else:
 		print("Error: unknown argument : {}".format(arg))
 		sys.exit(1)
