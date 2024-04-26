c209b190d929e99c78e2a65d695972eb3ae60058
Adrien Cassagne <adrien.cassagne@lip6.fr>
2024-04-26 21:38:18 +0200

    CI: Re-enable AVX-512 tests through Intel SDE simulator.

---
+++ ci/test-linux-intel-sde-unit.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -x
+
+if [ -z "$INTEL_SDE_ARCH" ]
+then
+	echo "Please define the 'INTEL_SDE_ARCH' environment variable (ex.: -skx)."
+	exit 1
+fi
+
+WD=$(pwd)
+mkdir softwares
+cd softwares
+wget https://largo.lip6.fr/monolithe/downloads/sde-external-9.33.0-2024-01-07-lin.tar.xz
+tar -xvvf sde-external-9.33.0-2024-01-07-lin.tar.xz
+ln -s $WD/softwares/sde-external-9.33.0-2024-01-07-lin $WD/softwares/sde
+export PATH=$WD/softwares/sde:$PATH
+cd ..
+
+for build in "$@"
+do
+	cd $build
+	sde64 $INTEL_SDE_ARCH -- ./bin/run-tests
+	rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+	cd ..
+done
