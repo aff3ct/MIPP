67958916fc65f6a4087ebb5d49bfb565e4d1fa84
Adrien Cassagne <adrien.cassagne@lip6.fr>
2024-04-26 17:01:20 +0200

    CI: Fix code coverage report generation.

--- ci/build-coverage-linux-armv8-gcc.sh
+++ ci/build-coverage-linux-armv8-gcc.sh
@@ -7,7 +7,7 @@ function gen_coverage_info {
 	build=$1
 	mkdir $build
 	cd $build
-	cmake .. -G"Unix Makefiles" -DCMAKE_CXX_COMPILER=g++ -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS_DEBUG="-g -O0" -DCMAKE_CXX_FLAGS="-Wall -funroll-loops -finline-functions --coverage $2" -DCMAKE_EXE_LINKER_FLAGS="--coverage" -DMIPP_STATIC_LIB=ON
+	cmake ../.. -G"Unix Makefiles" -DCMAKE_CXX_COMPILER=g++ -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS_DEBUG="-g -O0" -DCMAKE_CXX_FLAGS="-Wall -funroll-loops -finline-functions --coverage $2" -DCMAKE_EXE_LINKER_FLAGS="--coverage" -DMIPP_STATIC_LIB=ON
 	rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 	make -j $THREADS
 	rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
@@ -26,9 +26,9 @@ function gen_coverage_info {
 	fi
 }
 
-pip3 install --user -r requirements.txt
 python3 gencode.py
 
+cd tests
 mkdir code_coverage_files || true
 
 build_root=build_coverage_linux_armv8_gcc
--- ci/coverage-linux.sh
+++ ci/coverage-linux.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -x
 
+cd tests
 mkdir code_coverage_report || true
 
 genhtml code_coverage_files/* --output-directory ./code_coverage_report/
