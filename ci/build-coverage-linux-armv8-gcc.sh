8a31739532c002ceba8e31dd954676043a71053f
Adrien Cassagne <adrien.cassagne@lip6.fr>
2024-04-28 09:39:14 +0200

    New strategie: Use 'qemu' instead of 'armie' for SVE code coverage.

--- ci/build-coverage-linux-armv8-gcc.sh
+++ ci/build-coverage-linux-armv8-gcc.sh
@@ -7,7 +7,7 @@ function gen_coverage_info {
 	build=$1
 	mkdir $build
 	cd $build
-	cmake ../.. -G"Unix Makefiles" -DCMAKE_CXX_COMPILER=g++-11 -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS_DEBUG="-g -O0" -DCMAKE_CXX_FLAGS="-Wall -funroll-loops -finline-functions --coverage $2" -DCMAKE_EXE_LINKER_FLAGS="--coverage" -DMIPP_STATIC_LIB=ON
+	cmake ../.. -G"Unix Makefiles" -DCMAKE_CXX_COMPILER=g++ -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS_DEBUG="-g -O0" -DCMAKE_CXX_FLAGS="-Wall -funroll-loops -finline-functions --coverage $2" -DCMAKE_EXE_LINKER_FLAGS="--coverage" -DMIPP_STATIC_LIB=ON
 	rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 	make -j $THREADS
 	rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
@@ -16,8 +16,8 @@ function gen_coverage_info {
 		./bin/run-tests
 	else
 		# use Arm Instruction Emulator (armie) to execute the tests
-		source /usr/share/modules/init/profile.sh
-		module load armie22/22.0
+		# source /usr/share/modules/init/profile.sh
+		# module load armie22/22.0
 		if [[ "$build" == *"256"* ]]; then
 			nbits=256
 		else
@@ -28,7 +28,8 @@ function gen_coverage_info {
 				exit 1;
 			fi
 		fi
-		armie -msve-vector-bits=$nbits -- ./bin/run-tests
+		# armie -msve-vector-bits=$nbits -- ./bin/run-tests
+		qemu-aarch64 -cpu max,sve-default-vector-length=$nbits ./bin/run-tests
 	fi
 	cd ..
 	# rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
