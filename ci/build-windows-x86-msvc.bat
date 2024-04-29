13250220eb48eaa95f0bddd99f6d42e568ed23ea
Adrien Cassagne <adrien.cassagne@lip6.fr>
2024-04-29 15:15:47 +0200

    CI: Fix MSVC compilation.

--- ci/build-windows-x86-msvc.bat
+++ ci/build-windows-x86-msvc.bat
@@ -26,8 +26,8 @@ mkdir %build%
 cd %build%
 cmake .. -G"Visual Studio 15 2017 Win64" -DCMAKE_CXX_FLAGS="-D_CRT_SECURE_NO_DEPRECATE /EHsc /MP%THREADS% %params%" -DMIPP_STATIC_LIB=OFF
 if %ERRORLEVEL% neq 0 exit %ERRORLEVEL%
-rem devenv /build Release MIPP_tests.sln
-msbuild MIPP_tests.sln /t:Build /p:Configuration=Release
+rem devenv /build Release MIPP.sln
+msbuild MIPP.sln /t:Build /p:Configuration=Release
 if %ERRORLEVEL% neq 0 exit %ERRORLEVEL%
 move bin\Release\* bin\
 rmdir bin\Release\
