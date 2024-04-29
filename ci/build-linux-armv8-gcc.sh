1595de27e0a7346515e0a9288ba1191c02332536
Adrien Cassagne <adrien.cassagne@lip6.fr>
2024-04-29 15:12:26 +0200

    CI: Merge ARMv8 and ARMv8+SVE build jobs.

--- ci/build-linux-armv8-gcc.sh
+++ ci/build-linux-armv8-gcc.sh
@@ -18,5 +18,9 @@ pip3 install --user -r codegen/requirements.txt
 python3 codegen/gen_compress.py
 
 build_root=build_linux_armv8_gcc
-compile "${build_root}_nointr" "-DMIPP_NO_INTRINSICS"
-compile "${build_root}_neon"   "-march=armv8.1-a+simd"
+compile "${build_root}_nointr"     "-DMIPP_NO_INTRINSICS"
+compile "${build_root}_neon"       "-march=armv8.1-a+simd"
+compile "${build_root}_sve_ls128"  "-march=armv8-a+sve -msve-vector-bits=128"
+compile "${build_root}_sve_ls256"  "-march=armv8-a+sve -msve-vector-bits=256"
+compile "${build_root}_sve_ls512"  "-march=armv8-a+sve -msve-vector-bits=512"
+compile "${build_root}_sve_ls1024" "-march=armv8-a+sve -msve-vector-bits=1024"
