089b23469a2ecd49d98450f0a1250c1f822c1e36
Maxime Millet <maxime.millet@lip6.fr>
2025-06-20 17:31:29 +0200

    Simplify NEON interleave2 operations

--- include/mipp_impl_NEON.hxx
+++ include/mipp_impl_NEON.hxx
@@ -1425,46 +1425,34 @@
 	// ---------------------------------------------------------------------------------------------------- interleave2
 	template <>
 	inline regx2 interleave2<float>(const reg v1, const reg v2) {
-		uint32x2x2_t res1 = vzip_u32(vget_low_u32 ((uint32x4_t)v1), vget_low_u32 ((uint32x4_t)v2));
-		uint32x2x2_t res2 = vzip_u32(vget_high_u32((uint32x4_t)v1), vget_high_u32((uint32x4_t)v2));
+		float32x4x2_t zip = vzipq_f32(v1, v2);
 
-		regx2 res = {{(reg) vcombine_u32(res1.val[0], res2.val[0]),
-		              (reg) vcombine_u32(res1.val[1], res2.val[1])}};
-
-		return res;
+		return {{(reg) vcombine_f32(vget_low_f32(zip.val[0]), vget_low_f32(zip.val[1])),
+		         (reg) vcombine_f32(vget_high_f32(zip.val[0]), vget_high_f32(zip.val[1]))}};
 	}
 
 	template <>
 	inline regx2 interleave2<int32_t>(const reg v1, const reg v2) {
-		uint32x2x2_t res1 = vzip_u32(vget_low_u32 ((uint32x4_t)v1), vget_low_u32 ((uint32x4_t)v2));
-		uint32x2x2_t res2 = vzip_u32(vget_high_u32((uint32x4_t)v1), vget_high_u32((uint32x4_t)v2));
+		int32x4x2_t zip = vzipq_s32(v1, v2);
 
-		regx2 res = {{(reg) vcombine_u32(res1.val[0], res2.val[0]),
-		              (reg) vcombine_u32(res1.val[1], res2.val[1])}};
-
-		return res;
+		return {{(reg) vcombine_s32(vget_low_s32(zip.val[0]), vget_low_s32(zip.val[1])),
+		         (reg) vcombine_s32(vget_high_s32(zip.val[0]), vget_high_s32(zip.val[1]))}};
 	}
 
 	template <>
 	inline regx2 interleave2<int16_t>(const reg v1, const reg v2) {
-		uint16x4x2_t res1 = vzip_u16(vget_low_u16 ((uint16x8_t)v1), vget_low_u16 ((uint16x8_t)v2));
-		uint16x4x2_t res2 = vzip_u16(vget_high_u16((uint16x8_t)v1), vget_high_u16((uint16x8_t)v2));
+		int16x8x2_t zip = vzipq_s16(v1, v2);
 
-		regx2 res = {{(reg) vcombine_u16(res1.val[0], res2.val[0]),
-		              (reg) vcombine_u16(res1.val[1], res2.val[1])}};
-
-		return res;
+		return {{(reg) vcombine_s16(vget_low_s16(zip.val[0]), vget_low_s16(zip.val[1])),
+		         (reg) vcombine_s16(vget_high_s16(zip.val[0]), vget_high_s16(zip.val[1]))}};
 	}
 
 	template <>
 	inline regx2 interleave2<int8_t>(const reg v1, const reg v2) {
-		uint8x8x2_t res1 = vzip_u8(vget_low_u8 ((uint8x16_t)v1), vget_low_u8 ((uint8x16_t)v2));
-		uint8x8x2_t res2 = vzip_u8(vget_high_u8((uint8x16_t)v1), vget_high_u8((uint8x16_t)v2));
+		int8x16x2_t zip = vzipq_s8(v1, v2);
 
-		regx2 res = {{(reg) vcombine_u8(res1.val[0], res2.val[0]),
-		              (reg) vcombine_u8(res1.val[1], res2.val[1])}};
-
-		return res;
+		return {{(reg) vcombine_s8(vget_low_s8(zip.val[0]), vget_low_s8(zip.val[1])),
+		         (reg) vcombine_s8(vget_high_s8(zip.val[0]), vget_high_s8(zip.val[1]))}};
 	}
 
 	// ----------------------------------------------------------------------------------------------------------- andb
