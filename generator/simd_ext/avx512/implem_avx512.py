ff9e07f0e5e8cbef0274f6a6f9e8bb1249187f1a
Adrien Cassagne <adrien.cassagne@lip6.fr>
2026-03-22 18:29:21 +0100

    AVX-512: Try to fix reduce tpl.

--- generator/simd_ext/avx512/implem_avx512.py
+++ generator/simd_ext/avx512/implem_avx512.py
@@ -27,7 +27,7 @@ tpl_implem_avx512 = {
     "load":           { "format": "short", "code": "{{ isa.prefix }}_{{ instr_name }}_{{ isa_dt_par.data_ext_logi }}(({{ isa_dt_par.to_ptr }}*) p0);" },
     "store":          { "format": "short", "code": "{{ isa.prefix }}_{{ instr_name }}_{{ isa_dt_par.data_ext_logi }}(({{ isa_dt_par.to_ptr }}*) p0, r0.r);" },
     "set0":           { "format": "short", "code": "{{ isa.prefix }}_{{ instr_name }}_{{ isa_dt_par.data_ext_logi }}();"},
-    "set0_k":         { "format": "short", "code": "{{ isa.prefix }}_{{ instr_name }}_{{ isa_dt_par.data_ext_msk }}();" },
+    "set0_k":         { "format": "short", "code": "0;" },
     # "low_k":          { "format": "short", "code": "{{ isa.prefix }}_{{ instr_name }}_{{ isa_dt_par.data_ext_msk }}();" },
     "set1":           { "format": "short", "code": "{{ isa.prefix }}_{{ instr_name }}_{{ isa_dt_par.data_ext }}(v0);"},
     "arith_1arg":     { "format": "short", "code": "{{ isa.prefix }}_{{ instr_name }}_{{ isa_dt_par.data_ext }}(r0.r);" },
@@ -45,106 +45,121 @@ tpl_implem_avx512 = {
     "cmple_float":    { "format": "short", "code": "{{ isa.prefix }}_{{ instr_name }}_{{ isa_dt_par.data_ext }}_mask(r0.r, r1.r, _CMP_LE_OS);" },
     "cmpgt_float":    { "format": "short", "code": "{{ isa.prefix }}_{{ instr_name }}_{{ isa_dt_par.data_ext }}_mask(r0.r, r1.r, _CMP_GT_OS);" },
     "cmpge_float":    { "format": "short", "code": "{{ isa.prefix }}_{{ instr_name }}_{{ isa_dt_par.data_ext }}_mask(r0.r, r1.r, _CMP_GE_OS);" },
-    "round":          { "format": "short", "code": "{{ isa.prefix }}_{{ instr_name }}_{{ isa_dt_par.data_ext }}(r0.r, 0, _MM_FROUND_TO_NEAREST_INT | _MM_FROUND_NO_EXC);" },
+    "blend":          { "format": "short", "code": "{{ isa.prefix }}_mask_{{ instr_name }}_{{ isa_dt_par.data_ext }}(m0.m, r0.r, r1.r);" },
+    "round":          { "format": "short", "code": "{{ isa.prefix }}_{{ instr_name }}_{{ isa_dt_par.data_ext }}(r0.r, _MM_FROUND_TO_NEAREST_INT);" },
     "roundf":         { "format": "short", "code": "{{ isa.prefix }}_{{ instr_name }}_{{ isa_dt_par.data_ext }}(r0.r, _MM_FROUND_TO_NEAREST_INT | _MM_FROUND_NO_EXC, _MM_EXPADJ_NONE);" },
-    "blend":          { "format": "short", "code": "{{ isa.prefix }}_mask_{{ instr_name }}_{{ isa_dt_par.data_ext }}(r0.r, r1.r, m0.m);" },
     "getfirst": { "format": "long", "code":
 """// long format
 	return {{ isa.prefix }}_{{ instr_name }}(r0.r);""" },
     "reduce_64": { "format": "long", "code":
 """// long format
 	%r<c:float|b:32>% rsf;
-	rsf.r = _mm512_permutexvar_ps(%cast<tp,c:float|b:32>%(_mm512_set_epi32( 7, 6, 5, 4, 3, 2, 1, 0,15,14,13,12,11,10,9,8), %cast<tp,c:float|b:32>%(r0.r)));
+	%r<c:int|b:32>% rsi32;
+
+	rsf.r = _mm512_permutexvar_ps(_mm512_set_epi32( 7, 6, 5, 4, 3, 2, 1, 0,15,14,13,12,11,10,9,8), %cast<tp,c:float|b:32>%(r0).r);
 	%r<tp>% rs1 = %cast<c:float|b:32,tp>%(rsf);
 	rs1.r = {{ isa.prefix }}_{{ instr_name }}_{{ isa_dt_par.data_ext }}(r0.r, rs1.r);
-	rsf = %cast<tp,c:float|b:32>%(rs1);
-	rsf.r = _mm512_permutexvar_ps(%cast<tp,c:float|b:32>%(_mm512_set_epi32(11,10, 9, 8,15,14,13,12, 3, 2, 1, 0, 7, 6,5,4), %cast<tp,c:float|b:32>%(r0.r)));
+
+	rsf.r = _mm512_permutexvar_ps(_mm512_set_epi32(11,10, 9, 8,15,14,13,12, 3, 2, 1, 0, 7, 6,5,4), %cast<tp,c:float|b:32>%(rs1).r);
 	%r<tp>% rs2 = %cast<c:float|b:32,tp>%(rsf);
 	rs2.r = {{ isa.prefix }}_{{ instr_name }}_{{ isa_dt_par.data_ext }}(rs1.r, rs2.r);
-	rsf = %cast<tp,c:float|b:32>%(rs1);
-	rsf.r = _mm512_shuffle_epi32(rsf.r, _MM_PERM_ENUM(_MM_SHUFFLE(1,0,3,2)));
-	%r<tp>% rs3 = %cast<c:float|b:32,tp>%(rsf);
+
+	rsi32.r = _mm512_shuffle_epi32(rsi32.r, _MM_PERM_ENUM(_MM_SHUFFLE(1,0,3,2)));
+	%r<tp>% rs3 = %cast<c:int|b:32,tp>%(rsi32);
 	rs3.r = {{ isa.prefix }}_{{ instr_name }}_{{ isa_dt_par.data_ext }}(rs2.r, rs3.r);
+
 	return rs3;""" },
     "reduce_32": { "format": "long", "code":
 """// long format
 	%r<c:float|b:32>% rsf;
-	rsf.r = _mm512_permutexvar_ps(_mm512_set_epi32( 7, 6, 5, 4, 3, 2, 1, 0,15,14,13,12,11,10,9,8),%cast<tp,c:float|b:32>%(r0.r));
+	%r<c:int|b:32>% rsi32;
+
+	rsf.r = _mm512_permutexvar_ps(_mm512_set_epi32( 7, 6, 5, 4, 3, 2, 1, 0,15,14,13,12,11,10,9,8), %cast<tp,c:float|b:32>%(r0).r);
 	%r<tp>% rs1 = %cast<c:float|b:32,tp>%(rsf);
 	rs1.r = {{ isa.prefix }}_{{ instr_name }}_{{ isa_dt_par.data_ext }}(r0.r, rs1.r);
-	rsf = %cast<tp,c:float|b:32>%(rs1);
-	rsf.r = _mm512_permutexvar_ps(_mm512_set_epi32(11,10, 9, 8,15,14,13,12, 3, 2, 1, 0, 7, 6,5,4),%cast<tp,c:float|b:32>%(r0.r));
+
+	rsf.r = _mm512_permutexvar_ps(_mm512_set_epi32(11,10, 9, 8,15,14,13,12, 3, 2, 1, 0, 7, 6,5,4), %cast<tp,c:float|b:32>%(rs1).r);
 	%r<tp>% rs2 = %cast<c:float|b:32,tp>%(rsf);
 	rs2.r = {{ isa.prefix }}_{{ instr_name }}_{{ isa_dt_par.data_ext }}(rs1.r, rs2.r);
-	rsf = %cast<tp,c:float|b:32>%(rs2);
-	rsf.r = _mm512_shuffle_epi32(%cast<tp,c:float|b:32>%(r0).m,_MM_PERM_ENUM(_MM_SHUFFLE(1,0,3,2)));
-	%r<tp>% rs3 = %cast<c:float|b:32,tp>%(rsf);
+
+	rsi32.r = _mm512_shuffle_epi32(rsi32.r, _MM_PERM_ENUM(_MM_SHUFFLE(1,0,3,2)));
+	%r<tp>% rs3 = %cast<c:int|b:32,tp>%(rsi32);
 	rs3.r = {{ isa.prefix }}_{{ instr_name }}_{{ isa_dt_par.data_ext }}(rs2.r, rs3.r);
-	rsf = %cast<tp,c:float|b:32>%(rs3);
-	rsf.r = _mm512_shuffle_epi32(%cast<tp,c:float|b:32>%(r0).m,_MM_PERM_ENUM(_MM_SHUFFLE(1,0,3,2)));
-	%r<tp>% rs4 = %cast<c:float|b:32,tp>%(rsf);
+
+	rsi32.r = _mm512_shuffle_epi32(%cast<tp,c:int|b:32>%(rs3).r, _MM_PERM_ENUM(_MM_SHUFFLE(1,0,3,2)));
+	%r<tp>% rs4 = %cast<c:int|b:32,tp>%(rsi32);
 	rs4.r = {{ isa.prefix }}_{{ instr_name }}_{{ isa_dt_par.data_ext }}(rs3.r, rs4.r);
+
 	return rs4;""" },
     "reduce_16": { "format": "long", "code":
 """// long format
 	%r<c:float|b:32>% rsf;
-	rsf.r = _mm512_permutexvar_ps(_mm512_set_epi32( 7, 6, 5, 4, 3, 2, 1, 0,15,14,13,12,11,10,9,8),%cast<tp,c:float|b:32>%(r0.r));
-	%r<tp>% rs1 = %cast<c:float|b:32,tp>%(rsf);
-	rs1.r = {{ isa.prefix }}_{{ instr_name }}_{{ isa_dt_par.data_ext }}(r0.r, rs1.r);
-	rsf = %cast<tp,c:float|b:32>%(rs1);
-	rsf.r = _mm512_permutexvar_ps(_mm512_set_epi32(11,10, 9, 8,15,14,13,12, 3, 2, 1, 0, 7, 6,5,4),%cast<tp,c:float|b:32>%(r0.r));
-	%r<tp>% rs2 = %cast<c:float|b:32,tp>%(rsf);
-	rs2.r = {{ isa.prefix }}_{{ instr_name }}_{{ isa_dt_par.data_ext }}(rs1.r, rs2.r);
-	rsf = %cast<tp,c:float|b:32>%(rs2);
-	rsf.r = _mm512_shuffle_epi32(%cast<tp,c:float|b:32>%(r0).m,_MM_PERM_ENUM(_MM_SHUFFLE(1,0,3,2)));
-	rs3.r = {{ isa.prefix }}_{{ instr_name }}_{{ isa_dt_par.data_ext }}(rs2.r, rs3.r);
-	rsf = %cast<tp,c:float|b:32>%(rs3);
-	rsf.r = _mm512_shuffle_epi32(%cast<tp,c:float|b:32>%(r0).m,_MM_PERM_ENUM(_MM_SHUFFLE(1,0,3,2)));
-	%r<tp>% rs4 = %cast<c:float|b:32,tp>%(rsf);
-	rs4.r = {{ isa.prefix }}_{{ instr_name }}_{{ isa_dt_par.data_ext }}(rs3.r, rs4.r);
-	rsf = %cast<tp,c:float|b:32>%(rs4);
-	___m512i mask_16 = _mm512_set_epi8(61,60,63,62,57,56,59,58,53,52,55,54,49,48,51,50,
-	                                   45,44,47,46,41,40,43,42,37,36,39,38,33,32,35,34,
-	                                   29,28,31,30,25,24,27,26,21,20,23,22,17,16,19,18,
-	                                   13,12,15,14, 9, 8,11,10, 5, 4, 7, 6, 1, 0, 3, 2);
+	%r<c:int|b:32>% rsi32;
+	%r<c:int|b:8>% rsi8;
 
-	rsi.m = _mm512_shuffle_epi8(rsi.m, mask_16);
-	%r<tp>% rs5 = %cast<c:int|b:8,tp>%(rsi);
-	rs5.r = {{ isa.prefix }}_{{ instr_name }}_{{ isa_dt_par.data_ext }}(rs4.r, rs5.r);
-	return rs5;"""},
-    "reduce_8": { "format": "long", "code":
-"""// long format
-	%r<c:float|b:32>% rsf;
-	rsf.r = _mm512_permutexvar_ps(_mm512_set_epi32( 7, 6, 5, 4, 3, 2, 1, 0,15,14,13,12,11,10,9,8),%cast<tp,c:float|b:32>%(r0.r));
+	rsf.r = _mm512_permutexvar_ps(_mm512_set_epi32( 7, 6, 5, 4, 3, 2, 1, 0,15,14,13,12,11,10,9,8), %cast<tp,c:float|b:32>%(r0).r);
 	%r<tp>% rs1 = %cast<c:float|b:32,tp>%(rsf);
 	rs1.r = {{ isa.prefix }}_{{ instr_name }}_{{ isa_dt_par.data_ext }}(r0.r, rs1.r);
-	rsf = %cast<tp,c:float|b:32>%(rs1);
-	rsf.r = _mm512_permutexvar_ps(_mm512_set_epi32(11,10, 9, 8,15,14,13,12, 3, 2, 1, 0, 7, 6,5,4),%cast<tp,c:float|b:32>%(r0.r));
+
+	rsf.r = _mm512_permutexvar_ps(_mm512_set_epi32(11,10, 9, 8,15,14,13,12, 3, 2, 1, 0, 7, 6,5,4), %cast<tp,c:float|b:32>%(rs1).r);
 	%r<tp>% rs2 = %cast<c:float|b:32,tp>%(rsf);
 	rs2.r = {{ isa.prefix }}_{{ instr_name }}_{{ isa_dt_par.data_ext }}(rs1.r, rs2.r);
-	rsf = %cast<tp,c:float|b:32>%(rs2);
-	rsf.r = _mm512_shuffle_epi32(%cast<tp,c:float|b:32>%(r0).m,_MM_PERM_ENUM(_MM_SHUFFLE(1,0,3,2)));
-	%r<tp>% rs3 = %cast<c:float|b:32,tp>%(rsf);
+
+	rsi32.r = _mm512_shuffle_epi32(rsi32.r, _MM_PERM_ENUM(_MM_SHUFFLE(1,0,3,2)));
+	%r<tp>% rs3 = %cast<c:int|b:32,tp>%(rsi32);
 	rs3.r = {{ isa.prefix }}_{{ instr_name }}_{{ isa_dt_par.data_ext }}(rs2.r, rs3.r);
-	rsf = %cast<tp,c:float|b:32>%(rs3);
-	rsf.r = _mm512_shuffle_epi32(%cast<tp,c:float|b:32>%(r0).m,_MM_PERM_ENUM(_MM_SHUFFLE(2,3,0,1)));
-	%r<tp>% rs4 = %cast<c:float|b:32,tp>%(rsf);
+
+	rsi32.r = _mm512_shuffle_epi32(%cast<tp,c:int|b:32>%(rs3).r, _MM_PERM_ENUM(_MM_SHUFFLE(1,0,3,2)));
+	%r<tp>% rs4 = %cast<c:int|b:32,tp>%(rsi32);
 	rs4.r = {{ isa.prefix }}_{{ instr_name }}_{{ isa_dt_par.data_ext }}(rs3.r, rs4.r);
-	rsf = %cast<tp,c:float|b:32>%(rs4);
+
 	__m512i mask_16 = _mm512_set_epi8(61,60,63,62,57,56,59,58,53,52,55,54,49,48,51,50,
 	                                  45,44,47,46,41,40,43,42,37,36,39,38,33,32,35,34,
 	                                  29,28,31,30,25,24,27,26,21,20,23,22,17,16,19,18,
 	                                  13,12,15,14, 9, 8,11,10, 5, 4, 7, 6, 1, 0, 3, 2);
-	rsi.m = _mm512_shuffle_epi8(rsi.m, mask_16);
-	%r<tp>% rs5 = %cast<c:int|b:8,tp>%(rsi);
+	rsi8.r = _mm512_shuffle_epi8(%cast<tp,c:int|b:8>%(rs4).r, mask_16);
+	%r<tp>% rs5 = %cast<c:int|b:8,tp>%(rsi8);
 	rs5.r = {{ isa.prefix }}_{{ instr_name }}_{{ isa_dt_par.data_ext }}(rs4.r, rs5.r);
+
+	return rs5;"""},
+    "reduce_8": { "format": "long", "code":
+"""// long format
+	%r<c:float|b:32>% rsf;
+	%r<c:int|b:32>% rsi32;
+	%r<c:int|b:8>% rsi8;
+
+	rsf.r = _mm512_permutexvar_ps(_mm512_set_epi32( 7, 6, 5, 4, 3, 2, 1, 0,15,14,13,12,11,10,9,8), %cast<tp,c:float|b:32>%(r0).r);
+	%r<tp>% rs1 = %cast<c:float|b:32,tp>%(rsf);
+	rs1.r = {{ isa.prefix }}_{{ instr_name }}_{{ isa_dt_par.data_ext }}(r0.r, rs1.r);
+
+	rsf.r = _mm512_permutexvar_ps(_mm512_set_epi32(11,10, 9, 8,15,14,13,12, 3, 2, 1, 0, 7, 6,5,4), %cast<tp,c:float|b:32>%(rs1).r);
+	%r<tp>% rs2 = %cast<c:float|b:32,tp>%(rsf);
+	rs2.r = {{ isa.prefix }}_{{ instr_name }}_{{ isa_dt_par.data_ext }}(rs1.r, rs2.r);
+
+	rsi32.r = _mm512_shuffle_epi32(rsi32.r, _MM_PERM_ENUM(_MM_SHUFFLE(1,0,3,2)));
+	%r<tp>% rs3 = %cast<c:int|b:32,tp>%(rsi32);
+	rs3.r = {{ isa.prefix }}_{{ instr_name }}_{{ isa_dt_par.data_ext }}(rs2.r, rs3.r);
+
+	rsi32.r = _mm512_shuffle_epi32(%cast<tp,c:int|b:32>%(rs3).r, _MM_PERM_ENUM(_MM_SHUFFLE(1,0,3,2)));
+	%r<tp>% rs4 = %cast<c:int|b:32,tp>%(rsi32);
+	rs4.r = {{ isa.prefix }}_{{ instr_name }}_{{ isa_dt_par.data_ext }}(rs3.r, rs4.r);
+
+	__m512i mask_16 = _mm512_set_epi8(61,60,63,62,57,56,59,58,53,52,55,54,49,48,51,50,
+	                                  45,44,47,46,41,40,43,42,37,36,39,38,33,32,35,34,
+	                                  29,28,31,30,25,24,27,26,21,20,23,22,17,16,19,18,
+	                                  13,12,15,14, 9, 8,11,10, 5, 4, 7, 6, 1, 0, 3, 2);
+	rsi8.r = _mm512_shuffle_epi8(%cast<tp,c:int|b:8>%(rs4).r, mask_16);
+	%r<tp>% rs5 = %cast<c:int|b:8,tp>%(rsi8);
+	rs5.r = {{ isa.prefix }}_{{ instr_name }}_{{ isa_dt_par.data_ext }}(rs4.r, rs5.r);
+
 	__m512i mask_8  = _mm512_set_epi8(62,63,60,61,58,59,56,57,54,55,52,53,50,51,48,49,
 	                                  46,47,44,45,42,43,40,41,38,39,36,37,34,35,32,33,
 	                                  30,31,28,29,26,27,24,25,22,23,20,21,18,19,16,17,
 	                                  14,15,12,13,10,11, 8, 9, 6, 7, 4, 5, 2, 3, 0, 1);
-	rsi.m = _mm512_shuffle_epi8((rsi.m, mask_8));
-	%r<tp>% rs6 = %cast<c:int|b:8,tp>%(rsi);
+	rsi8.r = _mm512_shuffle_epi8(%cast<tp,c:int|b:8>%(rs5).r, mask_8);
+	%r<tp>% rs6 = %cast<c:int|b:8,tp>%(rsi8);
 	rs6.r = {{ isa.prefix }}_{{ instr_name }}_{{ isa_dt_par.data_ext }}(rs5.r, rs6.r);
+
 	return rs6;""" }
 }
 
@@ -179,7 +194,7 @@ implems_avx512 = {
     "sqrt": [
         { "instr_name": "sqrt",       "datatypes": all_float,                    "template": tpl_implem_avx512["arith_1arg"]                                                                                                    } ], # sqrt
     "rsqrt": [
-        { "instr_name": "rsqrt",      "datatypes": [float32],                    "template": tpl_implem_avx512["arith_1arg"]                                                                                                    } ], # rsqrt
+        { "instr_name": "rsqrt14",    "datatypes": [float64, float32],           "template": tpl_implem_avx512["arith_1arg"]                                                                                                    } ], # rsqrt
     "add": [
         { "instr_name": "add",        "datatypes": all_float + [int32],          "template": tpl_implem_avx512["arith_2args"]                                                                                                   },
         { "instr_name": "add",        "datatypes": [int64],                      "template": tpl_implem_avx512["arith_2args"]                                                                                                   },
