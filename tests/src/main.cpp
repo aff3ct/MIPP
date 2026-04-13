dbbf80d9aee7654444548bb925d1b4158eab1572
k-avi <lkingnavii@gmail.com>
2026-04-13 20:17:52 +0200

    Trying something

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -2,6 +2,16 @@
 #include <mipp.h>
 #include <catch2/catch_session.hpp>
 
+#if defined(__riscv_vector)
+__attribute__((constructor))
+static void force_enable_rvv() {
+  // Touch vector state early. This should not depend on any memory.
+  asm volatile ("vsetvli zero, zero, e8, m1, ta, ma" ::: "memory");
+}
+#endif
+
+
+
 int main(int argc, char* argv[])
 {
     std::cout << "MIPP tests" << std::endl;
