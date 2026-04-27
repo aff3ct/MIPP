4edfe9b2414b7369f15df7fefb608384b292817b
k-avi <lkingnavii@gmail.com>
2026-04-27 18:03:54 +0200

    Added different header gen support for avx512,neon

--- generator/simd_ext/avx512/gen_mipp_avx512.py
+++ generator/simd_ext/avx512/gen_mipp_avx512.py
@@ -12,7 +12,7 @@ from c_generator import *
 
 from include_gen import IncludeManager
 
-def gen_mipp_avx512(include_manager=None):
+def gen_mipp_avx512(include_manager):
 	for iemu in implems_emu_avx512:
 		for sub_iemu in implems_emu_avx512[iemu]:
 			if "type" not in sub_iemu:
@@ -20,24 +20,26 @@ def gen_mipp_avx512(include_manager=None):
 
 	# implementation C
 	
-	file = open("../include/avx512/mipp_impl_avx512_gen.h", "w")
-	
+	file_common = include_manager.get_fd(isa_avx512["name"], "common")
+ 	
 	tpl_header_avx = """#ifndef MY_INTRINSICS_PLUS_PLUS_IMPL_GEN_AVX512_H_
 #define MY_INTRINSICS_PLUS_PLUS_IMPL_GEN_AVX512_H_
 #include <immintrin.h>"""
 	j2_template = Template(tpl_header_avx, undefined=StrictUndefined)
-	print(j2_template.render(), file=file)
-	gen_c_defines(isa_avx512, file)
-	gen_c_structures(isa_avx512, file)
-	print("Generate AVX512")
-	copy_mipp_funcs = copy.deepcopy(mipp_funcs)
-	gen_c_functions(isa_avx512, file, copy_mipp_funcs, implems_avx512)
-	gen_c_functions(isa_avx512, file, copy_mipp_funcs, implems_emu_avx512)
-	gen_c_missing_functions(isa_avx512, file, copy_mipp_funcs)
-	
+	print(j2_template.render(), file=file_common)
+	gen_c_defines(isa_avx512, file_common)
+	gen_c_structures(isa_avx512, file_common)
+ 
 	tpl_footer_avx = """#endif /* MY_INTRINSICS_PLUS_PLUS_IMPL_GEN_AVX512_H_ */"""
 	j2_template = Template(tpl_footer_avx, undefined=StrictUndefined)
-	print(j2_template.render(), file=file)
-	
-	file.close()
+	print(j2_template.render(), file=file_common)
+ 
+	print("Generate AVX512")
+	copy_mipp_funcs = copy.deepcopy(mipp_funcs)
+	gen_c_functions(isa_avx512, include_manager, copy_mipp_funcs, implems_avx512)
+	gen_c_functions(isa_avx512, include_manager, copy_mipp_funcs, implems_emu_avx512)
+	gen_c_missing_functions(isa_avx512, include_manager, copy_mipp_funcs)
+ 
+	include_manager.resolve_all_dependencies(isa_avx512["name"], copy_mipp_funcs)
+	include_manager.create_glue_file(isa_avx512["name"], "../include/avx512/mipp_impl_avx512_gen.h")
 
--- generator/simd_ext/neon/gen_mipp_neon.py
+++ generator/simd_ext/neon/gen_mipp_neon.py
@@ -10,30 +10,33 @@ from c_generator import *
 from include_gen import IncludeManager
 
 
-def gen_mipp_neon(include_manager=None):
+def gen_mipp_neon(include_manager):
     for iemu in implems_emu_neon:
         for sub_iemu in implems_emu_neon[iemu]:
             if "type" not in sub_iemu:
                 sub_iemu["type"] = "emulated"
 
-    file = open("../include/neon/mipp_impl_neon_gen.h", "w")
+    file_common = include_manager.get_fd(isa_avx["name"], "common")
+
 
     tpl_header_neon = """#ifndef MY_INTRINSICS_PLUS_PLUS_IMPL_GEN_NEON_H_
 #define MY_INTRINSICS_PLUS_PLUS_IMPL_GEN_NEON_H_
 #include <arm_neon.h>"""
     j2_template = Template(tpl_header_neon, undefined=StrictUndefined)
-    print(j2_template.render(), file=file)
+    print(j2_template.render(), file=file_common)
 
-    gen_c_defines(isa_neon, file)
-    gen_c_structures(isa_neon, file)
-    print("Generate NEON")
-    copy_mipp_funcs = copy.deepcopy(mipp_funcs)
-    gen_c_functions(isa_neon, file, copy_mipp_funcs, implems_neon)
-    gen_c_functions(isa_neon, file, copy_mipp_funcs, implems_emu_neon)
-    gen_c_missing_functions(isa_neon, file, copy_mipp_funcs)
-
+    gen_c_defines(isa_neon, file_common)
+    gen_c_structures(isa_neon, file_common)
+    
     tpl_footer_neon = """#endif /* MY_INTRINSICS_PLUS_PLUS_IMPL_GEN_NEON_H_ */"""
     j2_template = Template(tpl_footer_neon, undefined=StrictUndefined)
-    print(j2_template.render(), file=file)
+    print(j2_template.render(), file=file_common)
+    
+    print("Generate NEON")
+    copy_mipp_funcs = copy.deepcopy(mipp_funcs)
+    gen_c_functions(isa_neon, include_manager, copy_mipp_funcs, implems_neon)
+    gen_c_functions(isa_neon, include_manager, copy_mipp_funcs, implems_emu_neon)
+    gen_c_missing_functions(isa_neon, include_manager, copy_mipp_funcs)
 
-    file.close()
+    include_manager.resolve_all_dependencies(isa_neon["name"], copy_mipp_funcs)
+    include_manager.create_glue_file(isa_neon["name"], "../include/neon/mipp_impl_neon_gen.h")
